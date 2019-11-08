b2646b5ce933d4a610770273f1a614913f57c74e
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-11-08 14:06:12 +0530

    Add files via upload

---
+++ gallery/1.js
@@ -0,0 +1,12 @@
+  const portfolioItems = document.querySelectorAll('.portfolio-item');
+        portfolioItems.forEach(portfolioItem => {
+            portfolioItem.addEventListener('mouseover', () => {
+              console.log(portfolioItem.childNodes[1].classList);
+                portfolioItem.childNodes[1].classList.add('img-darken');
+            });
+            
+             portfolioItem.addEventListener('mouseout', () => {
+              console.log(portfolioItem.childNodes[1].classList);
+                portfolioItem.childNodes[1].classList.remove('img-darken');
+            });
+        }); 
